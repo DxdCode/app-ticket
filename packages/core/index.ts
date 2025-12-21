1e82508aaceedf9ef309786cbadc233a8691a82c
DxdCode <davidmuela2407@gmail.com>
2025-12-20 19:42:05 -0500

    chore(core): export public API from core package

---
+++ packages/core/index.ts
@@ -0,0 +1,7 @@
+export { User, Token, Password, Register, Login } from "./src/auth";
+export { Ticket } from "./src/tickets";
+export { Message } from "./src/messages";
+export { AILog } from "./src/ai_logs";
+export { Examples } from "./src/examples";
+export { PromptA, PromptB, IA } from "./src/ia/promps";
+export { AppError } from "./src/shared/errors";
